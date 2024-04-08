91981a562f24dce516d41820e1f2e7f0bb081e26
wlkmanist <vlad.king5555@mail.ru>
2024-04-08 18:04:22 +0300

    Controller Fan and Auto Fans small improvements
    Signed-off-by: wlkmanist <vlad.king5555@mail.ru>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -586,6 +586,7 @@
   //#define CONTROLLER_FAN_USE_Z_ONLY       // With this option only the Z axis is considered
   //#define CONTROLLER_FAN_IGNORE_Z         // Ignore Z stepper. Useful when stepper timeout is disabled.
   #define CONTROLLERFAN_SPEED_MIN        30 // (0-255) Minimum speed. (If set below this value the fan is turned off.)
+  #define CONTROLLERFAN_SPEED_MAX       127 // (0-255) Maximum speed. (Use only for a specific hardware build.)
   #define CONTROLLERFAN_SPEED_ACTIVE    255 // (0-255) Active speed, used when any motor is enabled
   #define CONTROLLERFAN_SPEED_IDLE        0 // (0-255) Idle speed, used when motors are disabled
   #define CONTROLLERFAN_IDLE_TIME        60 // (seconds) Extra time to keep the fan running after disabling motors
