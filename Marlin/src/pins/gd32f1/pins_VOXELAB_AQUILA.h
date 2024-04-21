4449bb8d821ca254ce58580b2246deed31a99dd3
wlkmanist <vlad.king5555@mail.ru>
2024-04-22 00:07:27 +0300

    Revert "pins: Add unnecessary pins defenitions for stock Voxelab Aquila board"
    This reverts commit 57362a306c113c2499b45d3da2cbc4caf884a17c.

--- Marlin/src/pins/gd32f1/pins_VOXELAB_AQUILA.h
+++ Marlin/src/pins/gd32f1/pins_VOXELAB_AQUILA.h
@@ -27,45 +27,11 @@
  * Uses CREALITY V4 (STM32F103RE / STM32F103RC) board pin assignments
  */
 
-#ifndef BOARD_INFO_NAME
-  #define BOARD_INFO_NAME      "Aquila v1.0.1"
-#endif
+#define BOARD_INFO_NAME        "Aquila v1.0.1"
 #ifndef DEFAULT_MACHINE_NAME
   #define DEFAULT_MACHINE_NAME "Aquila"
 #endif
 
-#define NO_MAPLE_WARNING              // Disable warning when compiling with Maple env
-
-//
-// Power Loss Detection
-//
-//#ifndef POWER_LOSS_PIN
-//  #define POWER_LOSS_PIN      PA15  // PWRDET
-//#endif
-
-//#define LED                   PA3
-
-//
-// SDIO Pins
-//
-#define BOARD_SDIO_D0 			    PC8
-#define BOARD_SDIO_D1 			    PC9
-#define BOARD_SDIO_D2 			    PC10
-#define BOARD_SDIO_D3 			    PC11
-#define BOARD_SDIO_CLK 			    PC12
-#define BOARD_SDIO_CMD 			    PD2
-#define BOARD_SDIO_DET 			    PA10
-
-//
-// USARTS
-//
-#define BOARD_USART2_TX_PIN     PA9
-#define BOARD_USART2_RX_PIN     PA15
-
-#define BOARD_USART3_TX_PIN     PE5   // Debug
-#define BOARD_USART3_RX_PIN     PE4
-
-#define BOARD_USART4_TX_PIN     PC0
-#define BOARD_USART4_RX_PIN     PC1
+#define NO_MAPLE_WARNING     // Disable warning when compiling with Maple env
 
 #include "../stm32f1/pins_CREALITY_V4.h"
